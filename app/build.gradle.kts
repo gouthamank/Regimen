7bab6c54a86799b84c6838d486e1a4001c50ac5d
Gouthaman Kumar <gouthaman.k@gmail.com>
2026-07-15 22:50:13 -0400

    bump: version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "dev.gouthaman.regimen"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
